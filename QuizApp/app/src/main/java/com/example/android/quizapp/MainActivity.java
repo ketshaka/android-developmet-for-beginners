a43666f6cfb60fc5c7f285152c6903272db569f0
Tshaka Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-04-23 22:01:55 +0200

    add quiz app

---
+++ QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.android.quizapp;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button submit;
+    int score = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        submit = (Button) findViewById(R.id.submitButton);
+        submit.setOnClickListener(submitButtonOnClickListener);
+    }
+
+    private void checkQuesOne(){
+        RadioButton radioButtonYes = (RadioButton) findViewById(R.id.radioTrue);
+        boolean isQuesOneYesChecked = radioButtonYes.isChecked();
+        if (isQuesOneYesChecked){
+            score += 1;
+        }
+    }
+
+    private String getQuesTwoInput() {
+        EditText input = (EditText) findViewById(R.id.editQues2);
+        String algo = input.getText().toString();
+        return algo;
+    }
+
+    private void checkQuesTwo(){
+        String algo = getQuesTwoInput();
+        if (algo.trim().equalsIgnoreCase("recursion")){
+            score += 1;
+        }
+    }
+
+    private void checkQuesThree(){
+        CheckBox quesThreeQueues = (CheckBox) findViewById(R.id.checkQueues);
+        CheckBox quesThreeStacks = (CheckBox) findViewById(R.id.checkStacks);
+        CheckBox quesThreeTrees = (CheckBox) findViewById(R.id.checkTrees);
+        boolean isQuesThreeQueuesChecked = quesThreeQueues.isChecked();
+        boolean isQuesThreeStacksChecked = quesThreeStacks.isChecked();
+        boolean isQuesThreeTreesChecked = quesThreeTrees.isChecked();
+
+        if (isQuesThreeQueuesChecked && isQuesThreeStacksChecked && !isQuesThreeTreesChecked){
+            score += 1;
+        }
+    }
+
+    private void checkQuesFour(){
+        RadioButton radioButtonBubble = (RadioButton) findViewById(R.id.radioBubble);
+        boolean isQuesFourBubbleChecked = radioButtonBubble.isChecked();
+        if (isQuesFourBubbleChecked){
+            score += 1;
+        }
+    }
+
+    private void checkQues(){
+        checkQuesOne();
+        checkQuesTwo();
+        checkQuesThree();
+        checkQuesFour();
+    }
+
+    private void resetScore(){
+        score = 0;
+    }
+
+    final View.OnClickListener submitButtonOnClickListener = new View.OnClickListener() {
+        public void onClick(final View v){
+            checkQues();
+            Toast.makeText(MainActivity.this, "Whoop! ('.')/* " + score + "/4",
+                    Toast.LENGTH_LONG).show();
+            resetScore();
+        }
+    };
+}
