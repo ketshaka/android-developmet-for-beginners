bb4e9dc9895aecaf6025cba990cf53773c74107c
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-09-14 17:16:09 +0200

    add category activities

---
+++ HeartCapeTown/app/src/main/java/com/example/eric/heartcapetown/EatAndDrinkActivity.java
@@ -0,0 +1,25 @@
+package com.example.eric.heartcapetown;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class EatAndDrinkActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.word_list);
+
+        ArrayList<Attraction> attractions = new ArrayList<>();
+        attractions.add(new Attraction("Picture1", "Cool place", "www.coolplace.com"));
+        attractions.add(new Attraction("Picture2", "Okay place", "0254685"));
+
+        ListView attractionsListView = (ListView) findViewById(R.id.list);
+
+        AttractionAdapter adapter = new AttractionAdapter(this, attractions);
+
+        attractionsListView.setAdapter(adapter);
+    }
+}
---
+++ HeartCapeTown/app/src/main/java/com/example/eric/heartcapetown/NatureActivity.java
@@ -0,0 +1,6 @@
+package com.example.eric.heartcapetown;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class NatureActivity extends AppCompatActivity {
+}
