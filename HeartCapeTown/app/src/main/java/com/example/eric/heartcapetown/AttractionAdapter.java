611604a92c51ad90c6a8bb5c24846f60080b9a85
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-09-15 08:33:37 +0200

    update attraction adapter
    - add comments
    - add image view to adapter

--- HeartCapeTown/app/src/main/java/com/example/eric/heartcapetown/AttractionAdapter.java
+++ HeartCapeTown/app/src/main/java/com/example/eric/heartcapetown/AttractionAdapter.java
@@ -5,33 +5,58 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
 public class AttractionAdapter extends ArrayAdapter<Attraction> {
 
+    /**
+     * Create a new {@link AttractionAdapter} object
+     *
+     * @param context is the current context (activity) that the adapter is being created
+     * @param attractions is the list of {@link Attraction} to be displayed
+     */
     public AttractionAdapter(Context context, List<Attraction> attractions) {
         super(context, 0, attractions);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if existing view is being reused, otherwise inflate a new view
         View listItemView = convertView;
         if (listItemView == null) {
-            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.attraction_list_item, parent, false);
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.attraction_list_item, parent, false);
         }
 
+        // Get the {@link Attraction} object located at this position in the list
         Attraction currentAttraction = getItem(position);
 
+        // Find the TextView in the attraction_list_item.xml layout with the ID heading
         TextView headingView = (TextView) listItemView.findViewById(R.id.heading);
+        // Get the heading from the currentAttraction object and set this text on the heading
+        // TextView
         headingView.setText(currentAttraction.getHeading());
 
+        // Find the TextView in the attraction_list_item.xml layout with the ID description
         TextView descriptionView = (TextView) listItemView.findViewById(R.id.description);
+        // Get the description from the currentAttraction object and set this text on the
+        // description TextView
         descriptionView.setText(currentAttraction.getDescription());
 
+        // Find the TextView in the attraction_list_item.xml layout with the ID contacts
         TextView contactsView = (TextView) listItemView.findViewById(R.id.contacts);
+        // Get the contacts from the currentAttraction object and set this text on the contacts
+        // TextView
         contactsView.setText(currentAttraction.getContacts());
+
+        // Find the ImageView in the attraction_list_item.xml layout with the ID picture
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.picture);
+        // Get the image resource ID from the currentAttraction object and display provided image
+        // based on the resource ID
+        imageView.setImageResource(currentAttraction.getImageResourceId());
 
         return listItemView;
 
