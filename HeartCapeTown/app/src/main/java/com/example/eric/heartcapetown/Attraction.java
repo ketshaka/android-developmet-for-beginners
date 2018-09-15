c1ae3400b53f485c46630493e4701da2e81d5609
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-09-15 08:32:16 +0200

    update attraction class
    - add comments
    - add image resource id variable

--- HeartCapeTown/app/src/main/java/com/example/eric/heartcapetown/Attraction.java
+++ HeartCapeTown/app/src/main/java/com/example/eric/heartcapetown/Attraction.java
@@ -2,16 +2,33 @@ package com.example.eric.heartcapetown;
 
 public class Attraction {
 
-    private String mHeading;
-    private String mDescription;
-    private String mContacts;
 
-    public Attraction(String heading, String description, String contacts) {
+    // Name of the attraction
+    private String mHeading;
+    // Description of the attraction
+    private String mDescription;
+    // Contact details of the attraction
+    private String mContacts;
+    // Image resource ID for the attraction
+    private int mImageResourceId;
+
+    /**
+     * Public constructor
+     * @param heading is the name of the tourist attraction
+     * @param description is the description of the tourist attraction
+     * @param contacts is the contact details of the tourist attraction
+     * @param imageResourceId is a drawable resource ID for the picture of the tourist attraction
+     */
+    public Attraction(String heading, String description, String contacts, int imageResourceId) {
         mHeading = heading;
         mDescription = description;
         mContacts = contacts;
+        mImageResourceId = imageResourceId;
     }
 
+    /**
+     *Public getter methods for the above private methods
+     */
     public String getHeading() {
         return mHeading;
     }
@@ -22,5 +39,9 @@ public class Attraction {
 
     public String getContacts() {
         return mContacts;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
     }
 }
