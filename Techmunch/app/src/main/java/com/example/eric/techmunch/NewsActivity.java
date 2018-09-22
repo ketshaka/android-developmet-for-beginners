857eb956b601a71c8499361aafa9d60ffec3508a
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-09-22 22:21:01 +0200

    Comment out fake news list

--- Techmunch/app/src/main/java/com/example/eric/techmunch/NewsActivity.java
+++ Techmunch/app/src/main/java/com/example/eric/techmunch/NewsActivity.java
@@ -13,14 +13,16 @@ public class NewsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ArrayList<News> news = QueryUtils.extractNews();
+
         // Create fake list of news
-        ArrayList<News> news = new ArrayList<>();
-        news.add(new News("Consumer", "New Kalahari Echo device leaked", "Kris Ball", "October 20, 2019"));
-        news.add(new News("Fintech", "Debit cards used on Oldegg.com conpromised", "Jack Leadbitter", "October 20, 2019"));
-        news.add(new News("Consumer", "Tenpounds Video is going private in the UK", "Julius Moretti, Julia Hu", "October 20, 2019"));
-        news.add(new News("Consumer", "MeTube Gaminng is shutting down", "Patrick Fernandez", "October 19, 2019"));
-        news.add(new News("Transport","Postfriends is now a unicorn", "Alan Labinsky", "October 19, 2019"));
-        news.add(new News("Social", "Way is shutting down", "Bjorn Tussel", "October 19, 2019"));
+        // ArrayList<News> news = new ArrayList<>();
+        // news.add(new News("Consumer", "New Kalahari Echo device leaked", "Kris Ball", "October 20, 2019"));
+        // news.add(new News("Fintech", "Debit cards used on Oldegg.com conpromised", "Jack Leadbitter", "October 20, 2019"));
+        // news.add(new News("Consumer", "Tenpounds Video is going private in the UK", "Julius Moretti, Julia Hu", "October 20, 2019"));
+        // news.add(new News("Consumer", "MeTube Gaminng is shutting down", "Patrick Fernandez", "October 19, 2019"));
+        // news.add(new News("Transport","Postfriends is now a unicorn", "Alan Labinsky", "October 19, 2019"));
+        // news.add(new News("Social", "Way is shutting down", "Bjorn Tussel", "October 19, 2019"));
 
         // Find a reference to the {@link ListView} in the layout
         ListView newsListView = (ListView) findViewById(R.id.list);
