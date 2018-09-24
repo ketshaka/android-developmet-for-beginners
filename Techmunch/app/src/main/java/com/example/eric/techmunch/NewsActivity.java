9e231413ac9c58f02bebde37f3c58fdfdb413e5f
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-09-24 21:34:56 +0200

    Add onClickListener to link views
    - Add onClickListener to views
    - Modify adapter to final so that we can access onClickListener with the adapter

--- Techmunch/app/src/main/java/com/example/eric/techmunch/NewsActivity.java
+++ Techmunch/app/src/main/java/com/example/eric/techmunch/NewsActivity.java
@@ -1,7 +1,11 @@
 package com.example.eric.techmunch;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -28,9 +32,26 @@ public class NewsActivity extends AppCompatActivity {
         ListView newsListView = (ListView) findViewById(R.id.list);
 
         // Create a new adapter that takes the list of news as input
-        NewsAdapter adapter = new NewsAdapter(this, news);
+        final NewsAdapter adapter = new NewsAdapter(this, news);
 
         // Set the adapter on the {@link ListView}
         newsListView.setAdapter(adapter);
+
+        newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current earthquake that was clicked on
+                News currentNews = adapter.getItem(position);
+
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri newsUri = Uri.parse(currentNews.getUrl());
+
+                // Create a new intent to view the earthquake URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
+
+                // Send the intent to launch a new activity
+                startActivity(websiteIntent);
+            }
+        });
     }
 }
