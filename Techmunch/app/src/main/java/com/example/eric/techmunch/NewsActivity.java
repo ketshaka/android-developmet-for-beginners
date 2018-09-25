15c3beb208be79056996ec2df9deadd08df50e47
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-09-26 01:20:32 +0200

    Add loader calls

--- Techmunch/app/src/main/java/com/example/eric/techmunch/NewsActivity.java
+++ Techmunch/app/src/main/java/com/example/eric/techmunch/NewsActivity.java
@@ -1,23 +1,48 @@
 package com.example.eric.techmunch;
 
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class NewsActivity extends AppCompatActivity {
+public class NewsActivity extends AppCompatActivity
+        implements LoaderCallbacks<List<News>> {
+
+    private static final String LOG_TAG = NewsActivity.class.getName();
+
+    // URL for news stories from the Guardian API
+    private static final String NEWS_REQUEST_URL =
+            "https://content.guardianapis.com/search?q=technology&api-key=bad0b58c-fbb8-4357-b899-be366479cc67";
+
+     // Constant value for the news loader ID. We can choose any integer.
+     // This really only comes into play if you're using multiple loaders.
+    private static final int NEWS_LOADER_ID = 1;
+
+    // Adapter for the list of news
+    private NewsAdapter mAdapter;
+
+    // TextView that is displayed when the list is empty
+    private TextView mEmptyStateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<News> news = QueryUtils.extractNews();
+        // ArrayList<News> news = QueryUtils.extractNews();
 
         // Create fake list of news
         // ArrayList<News> news = new ArrayList<>();
@@ -32,26 +57,85 @@ public class NewsActivity extends AppCompatActivity {
         ListView newsListView = (ListView) findViewById(R.id.list);
 
         // Create a new adapter that takes the list of news as input
-        final NewsAdapter adapter = new NewsAdapter(this, news);
+        //final NewsAdapter adapter = new NewsAdapter(this, news);
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
 
         // Set the adapter on the {@link ListView}
-        newsListView.setAdapter(adapter);
+        newsListView.setAdapter(mAdapter);
 
         newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                // Find the current earthquake that was clicked on
-                News currentNews = adapter.getItem(position);
+                // Find the current news story that was clicked on
+                News currentNews = mAdapter.getItem(position);
 
                 // Convert the String URL into a URI object (to pass into the Intent constructor)
                 Uri newsUri = Uri.parse(currentNews.getUrl());
 
-                // Create a new intent to view the earthquake URI
+                // Create a new intent to view the news URI
                 Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
 
                 // Send the intent to launch a new activity
                 startActivity(websiteIntent);
             }
         });
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+             mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+    }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
+        // Create a new loader for the given URL
+        return new NewsLoader(this, NEWS_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> stories) {
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "No news found."
+        mEmptyStateTextView.setText(R.string.no_news);
+
+        // Clear the adapter of previous news data
+        mAdapter.clear();
+
+        // If there is a valid list of {@link News}, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+        if (stories != null && !stories.isEmpty()) {
+            mAdapter.addAll(stories);
+            //updateUi(stories);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
     }
 }
