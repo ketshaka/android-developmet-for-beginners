7a0ccc7814c1b218b3387788e9d2bffd019628e0
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-10-01 11:53:57 +0200

    Add intent to EditorActivity

--- Notepad/app/src/main/java/com/example/eric/notepad/MainActivity.java
+++ Notepad/app/src/main/java/com/example/eric/notepad/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.eric.notepad;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,12 +20,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Set up FAB to open EditorActivity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                  //      .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(intent);
             }
         });
     }
