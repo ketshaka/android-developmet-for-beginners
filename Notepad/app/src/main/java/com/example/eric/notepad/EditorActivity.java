425587acd6b5458673bbe08da19fe174e72a6f59
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-10-02 12:21:34 +0200

    Update EditorActivity Java class
    - Add editor activity menu
    - Add menu options (not functional yet)

--- Notepad/app/src/main/java/com/example/eric/notepad/EditorActivity.java
+++ Notepad/app/src/main/java/com/example/eric/notepad/EditorActivity.java
@@ -1,10 +1,15 @@
 package com.example.eric.notepad;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
+/**
+ *  Allows user to create a new note or edit an existing one.
+ */
 public class EditorActivity extends AppCompatActivity {
 
     // EditText field to enter the note's heading
@@ -20,6 +25,35 @@ public class EditorActivity extends AppCompatActivity {
         // Find relevant views that user input will be read from
         mHeadingEditText = (EditText) findViewById(R.id.heading);
         mBodyEditText = (EditText) findViewById(R.id.body);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+                // Navigate back to parent activity (CatalogActivity)
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
