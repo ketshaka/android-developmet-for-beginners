c5d73dbf6b032789edff12facc59d1ef5d5fdc81
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-10-05 14:21:56 +0200

    Add insertNot method

--- Notepad/app/src/main/java/com/example/eric/notepad/EditorActivity.java
+++ Notepad/app/src/main/java/com/example/eric/notepad/EditorActivity.java
@@ -1,21 +1,27 @@
 package com.example.eric.notepad;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.eric.notepad.data.NoteContract;
+import com.example.eric.notepad.data.NoteDbHelper;
 
 /**
  *  Allows user to create a new note or edit an existing one.
  */
 public class EditorActivity extends AppCompatActivity {
 
-    // EditText field to enter the note's heading
-    private EditText mHeadingEditText;
+    // EditText field to enter the note's title
+    private EditText mTitleEditText;
     // EditText field to enter the note's body
-    private EditText mBodyEditText;
+    private EditText mNoteEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +29,41 @@ public class EditorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_editor);
 
         // Find relevant views that user input will be read from
-        mHeadingEditText = (EditText) findViewById(R.id.heading);
-        mBodyEditText = (EditText) findViewById(R.id.body);
+        mTitleEditText = (EditText) findViewById(R.id.title);
+        mNoteEditText = (EditText) findViewById(R.id.note);
     }
 
+    /**
+     * Get user input from editor and save a new note into the database.
+     */
+    private void insertNote() {
+        // Read from input fields
+        String titleString = mTitleEditText.getText().toString();
+        String noteString = mNoteEditText.getText().toString();
+
+        // Create database helper
+        NoteDbHelper mDbHelper = new NoteDbHelper(this);
+
+        // Gets the database in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a ContentValues object where the column names are they keys, and the note's
+        // content the values.
+        ContentValues values = new ContentValues();
+        values.put(NoteContract.NoteEntry.COLUMN_TITLE, titleString);
+        values.put(NoteContract.NoteEntry.COLUMN_NOTE, noteString);
+
+        // Insert a new row for a note in the database, returning the ID of that new row.
+        long newRowId = db.insert(NoteContract.NoteEntry.TABLE_NAME, null, values);
+        // Show a toast message depending on whether or not the insertion was successful
+        if (newRowId == -1) {
+            // If the row ID is -1, then there was an error with insertion.
+            Toast.makeText(this, "Error with saving note", Toast.LENGTH_SHORT).show();
+        } else {
+            // Otherwise, the insertion was successful and we can display a toast with the row ID.
+            Toast.makeText(this, "Note saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_editor.xml file.
@@ -41,7 +78,10 @@ public class EditorActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to a click on the "Save" menu option
             case R.id.action_save:
-                // Do nothing for now
+                // Save note to database
+                insertNote();
+                // Exit activity
+                finish();
                 return true;
             // Respond to a click on the "Delete" menu option
             case R.id.action_delete:
