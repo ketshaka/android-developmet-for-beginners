2fcc64a5dcf39c04fa3e818e38ab8217fb830dc1
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-10-05 14:21:12 +0200

    Update NoteContract class

--- Notepad/app/src/main/java/com/example/eric/notepad/data/NoteContract.java
+++ Notepad/app/src/main/java/com/example/eric/notepad/data/NoteContract.java
@@ -1,6 +1,36 @@
 package com.example.eric.notepad.data;
 
-public final class NoteContract { // final so that it cannot be extended
+import android.provider.BaseColumns;
 
-    
+/**
+ * API Contract for the Notepad app.
+ */
+public final class NoteContract {
+        // final so that it cannot be extended. It is just a class for implementing constants
+
+    // To prevent someone from accidentally instantiating the contract class, give it an empty
+    // constructor.
+    private NoteContract() {}
+
+    /**
+     * Inner class that defines the constant values for the notes database table. Each entry in the
+     * table represents a single note.
+     */
+    public static final class NoteEntry implements BaseColumns {
+
+        //Name of the notes database table
+        public final static String TABLE_NAME = "notes";
+
+        // Unique ID number for the note (only for use in the database table).
+        // Type: INTEGER PRIMARY KEY AUTOINCREMENT NOT
+        public final static String _ID = BaseColumns._ID;
+
+        // Title of the note
+        // Type: TEXT
+        public final static String COLUMN_TITLE = "title";
+
+        // Body of the note
+        //Type: TEXT
+        public final static String COLUMN_NOTE =  "note";
+    }
 }
