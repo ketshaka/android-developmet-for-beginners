46aa0e510e5e9b8748ae82d6ba2da99dec058454
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-10-08 20:35:54 +0200

    Add URIs to the Contract

--- Notepad/app/src/main/java/com/example/eric/notepad/data/NoteContract.java
+++ Notepad/app/src/main/java/com/example/eric/notepad/data/NoteContract.java
@@ -1,5 +1,6 @@
 package com.example.eric.notepad.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -13,10 +14,35 @@ public final class NoteContract {
     private NoteContract() {}
 
     /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.eric.notepad";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.eric.notepad/notes/ is a valid path for looking at note
+     * data. content://com.example.eric.notepad/staff/ will fail, as the ContentProvider hasn't been
+     * given any information on what to do with "staff".
+     */
+    public static final String PATH_NOTES = "notes";
+
+    /**
      * Inner class that defines the constant values for the notes database table. Each entry in the
      * table represents a single note.
      */
     public static final class NoteEntry implements BaseColumns {
+
+        // The content URI to access the note data in the provider
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_NOTES);
 
         //Name of the notes database table
         public final static String TABLE_NAME = "notes";
