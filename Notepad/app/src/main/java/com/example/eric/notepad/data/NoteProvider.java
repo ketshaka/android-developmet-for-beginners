75123ec2a2130f09b8a65f7d3f684a6f37bf22cb
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-10-08 20:36:28 +0200

    Add URI Matcher

--- Notepad/app/src/main/java/com/example/eric/notepad/data/NoteProvider.java
+++ Notepad/app/src/main/java/com/example/eric/notepad/data/NoteProvider.java
@@ -1,11 +1,48 @@
 package com.example.eric.notepad.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 public class NoteProvider extends ContentProvider {
+
+    /** URI matcher code for the content URI for the notes table */
+    private static final int NOTES = 100;
+
+    /** URI matcher code for the content URI for a single note in the notes table */
+    private static final int NOTE_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+
+        // The content URI of the form "content://com.example.eric.notepad/notes" will map to the
+        // integer code {@link #NOTES}. This URI is used to provide access to MULTIPLE rows
+        // of the notes table.
+        sUriMatcher.addURI(NoteContract.CONTENT_AUTHORITY, NoteContract.PATH_NOTES, NOTES);
+
+        // The content URI of the form "content://com.example.eric.notepad/notes/#" will map to the
+        // integer code {@link #NOTE_ID}. This URI is used to provide access to ONE single row
+        // of the notes table.
+        //
+        // In this case, the "#" wildcard is used where "#" can be substituted for an integer.
+        // For example, "content://com.example.eric.notepad/notes/3" matches, but
+        // "content://com.example.eric.notepad/notes" (without a number at the end) doesn't match.
+        sUriMatcher.addURI(NoteContract.CONTENT_AUTHORITY, NoteContract.PATH_NOTES + "/#", NOTE_ID);
+    }
 
     /** Database helper object */
     private NoteDbHelper mDbHelper;
@@ -26,7 +63,50 @@ public class NoteProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        return null;
+
+        // Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        // This cursor will hold the result of the query
+        Cursor cursor;
+
+        // Figure out if the URI matcher can match the URI to a specific code
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case NOTES:
+                // For the NOTES code, query the notes table directly with the given projection,
+                // selection, selection arguments, and sort order. The cursor could contain multiple
+                // rows of the notes table.
+                cursor = database.query(NoteContract.NoteEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            case NOTE_ID:
+                // For the NOTE_ID code, extract out the ID from the URI.
+                // For an example URI such as "content://com.example.eric.notepad/notes/3",
+                // the selection will be "_id=?" and the selection argument will be a String array
+                // containing the actual ID of 3 in this case.
+                //
+                // For every "?" in the selection, we need to have an element in the selection
+                // arguments that will fill in the "?". Since we have 1 question mark in the
+                // selection, we have 1 String in the selection arguments' String array.
+                selection = NoteContract.NoteEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                // This will perform a query on the notes table where the _id equals 3 to return a
+                // Cursor containing that row of the table.
+                cursor = database.query(NoteContract.NoteEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+
+        // Set notification URI on the Cursor, so we know what content URI the Cursor was created
+        // for. If the data at this URI changes, then we know we need to update the Cursor.
+        // cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        // Return the cursor
+        return cursor;
     }
 
     /**
