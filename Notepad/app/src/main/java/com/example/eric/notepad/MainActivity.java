9e0270845376e5389c8645c6103eea9418cb5016
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-10-12 15:44:06 +0200

    Update MainActivity
    - Add list view template reference
    - Remove projection
    - Add intents for touch views and deletes
    - CursorLoader functionality

--- Notepad/app/src/main/java/com/example/eric/notepad/MainActivity.java
+++ Notepad/app/src/main/java/com/example/eric/notepad/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.eric.notepad;
 
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,13 +9,20 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -25,7 +33,11 @@ import com.example.eric.notepad.data.NoteDbHelper;
  *  Displays notes that were entered and stored in the app.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+
+    // Identifier for the note data loader
+    private static final int NOTE_LOADER = 0;
 
     // Adapter for the ListView
     NoteCursorAdapter mCursorAdapter;
@@ -34,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -46,52 +60,44 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        displayDatabaseInfo();
-    }
-
-    /**
-     * Temporary helper method to display information in the onscreen TextView about the state of
-     * the notes database.
-     */
-    private void displayDatabaseInfo() {
-
-        /**
-         *  Old query method. That could facilitate injection attacks.
-         */
-        // Perform this raw SQL query "SELECT * FROM notes"
-        // to get a Cursor that contains all rows from the notes table.
-        // Cursor cursor = db.rawQuery("SELECT * FROM " + NoteContract.NoteEntry.TABLE_NAME, null);
-
-        // Define projection that specifies which columns from the database will be used after this
-        // query.
-        String[] projection = {
-                NoteContract.NoteEntry._ID,
-                NoteContract.NoteEntry.COLUMN_TITLE,
-                NoteContract.NoteEntry.COLUMN_NOTE
-        };
-
-        // Perform a query on the provider using the ContentResolver.
-        // Use the {@link NoteContract.NoteEntry#CONTENT_URI} to access the note data.
-        Cursor cursor = getContentResolver().query(
-                NoteContract.NoteEntry.CONTENT_URI, // content URI of the table
-                projection, // columns to return
-                null, // selection criteria
-                null, // selection criteria
-                null); // sort order
 
         // Find the ListView which will be populated with the note data
         ListView noteListView = (ListView) findViewById(R.id.list);
 
-        // Setup an Adapter to create a list item for each row of pet data in the Cursor.
-        NoteCursorAdapter adapter = new NoteCursorAdapter(this, cursor);
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        // View emptyView = findViewById(R.id.empty_view);
+        // noteListView.setEmptyView(emptyView);
 
-        // Attach the adapter to the ListView
-        noteListView.setAdapter(adapter);
+        // Setup an Adapter to create a list item for each row of note data in the Cursor.
+        // There is no note data yet (until the loader finishes) so pass in null for the Cursor.
+        mCursorAdapter = new NoteCursorAdapter(this, null);
+        noteListView.setAdapter(mCursorAdapter);
+
+        // Setup the item click listener
+        noteListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Create new intent to go to {@link EditorActivity}
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+
+                // Form the content URI that represents the specific note that was clicked on,
+                // by appending the "id" (passed as input to this method) onto the
+                // {@link NoteEntry#CONTENT_URI}.
+                // For example, the URI would be "content://com.example.eric.notepad/notes/2"
+                // if the pet with ID 2 was clicked on.
+                Uri currentNoteUri = ContentUris.withAppendedId(
+                        NoteContract.NoteEntry.CONTENT_URI, id);
+
+                // Set the URI on the data field of the intent
+                intent.setData(currentNoteUri);
+
+                // Launch the {@link EditorActivity} to display the data for the current note.
+                startActivity(intent);
+            }
+        });
+
+        // Start the loader
+        getLoaderManager().initLoader(NOTE_LOADER, null, this);
     }
 
     /**
@@ -125,6 +131,14 @@ public class MainActivity extends AppCompatActivity {
         Uri newUri = getContentResolver().insert(NoteContract.NoteEntry.CONTENT_URI, values);
     }
 
+    /**
+     * Helper method to delete all notes in the database.
+     */
+    private void deleteAllNotes() {
+        int rowsDeleted = getContentResolver().delete(
+                NoteContract.NoteEntry.CONTENT_URI, null, null);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_main.xml file.
@@ -140,13 +154,42 @@ public class MainActivity extends AppCompatActivity {
             // Respond to a click on the "Insert dummy data" menu option
             case R.id.action_insert_dummy_data:
                 insertNote();
-                displayDatabaseInfo();
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_notes:
-                // Do nothing for now
+                deleteAllNotes();
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Define a projection that specifies the columns from the table we care about.
+        String[] projection = {
+                NoteContract.NoteEntry._ID,
+                NoteContract.NoteEntry.COLUMN_TITLE,
+                NoteContract.NoteEntry.COLUMN_NOTE };
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                NoteContract.NoteEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Update {@link NoteCursorAdapter} with this new cursor containing updated note data
+        mCursorAdapter.swapCursor(data);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Callback called when the data needs to be deleted
+        mCursorAdapter.swapCursor(null);
     }
 }
