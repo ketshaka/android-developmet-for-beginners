25f41ae5189eefeae57c2674b79ca6e2641fe9d6
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-10-12 15:41:50 +0200

    Update EditorActivity
    - Add CursorLoader
    - Add TouchListener for edits

--- Notepad/app/src/main/java/com/example/eric/notepad/EditorActivity.java
+++ Notepad/app/src/main/java/com/example/eric/notepad/EditorActivity.java
@@ -1,13 +1,25 @@
 package com.example.eric.notepad;
 
 import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.app.NavUtils;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,30 +29,91 @@ import com.example.eric.notepad.data.NoteDbHelper;
 /**
  *  Allows user to create a new note or edit an existing one.
  */
-public class EditorActivity extends AppCompatActivity {
+public class EditorActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+
+    // Identifier for the notes data loader
+    private static final int EXISTING_NOTE_LOADER = 0;
+
+    /** Content URI for the existing note (null if it's a new note) */
+    private Uri mCurrentNoteUri;
 
     // EditText field to enter the note's title
     private EditText mTitleEditText;
     // EditText field to enter the note's body
     private EditText mNoteEditText;
 
+    /** Boolean flag that keeps track of whether the note has been edited (true) or not (false) */
+    private boolean mNoteHasChanged = false;
+
+    /**
+     * OnTouchListener that listens for any user touches on a View, implying that they are modifying
+     * the view, and we change the mPetHasChanged boolean to true.
+     */
+    private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            mNoteHasChanged = true;
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
 
+        // Examine the intent that was used to launch this activity, in order to figure out if we're
+        // creating a new note or editing an existing one.
+        Intent intent = getIntent();
+        mCurrentNoteUri = intent.getData();
+
+        // If the intent DOES NOT contain a note content URI, then we know that we are
+        // creating a new note.
+        if (mCurrentNoteUri == null) {
+            // This is a new note, so change the app bar to say "Add a Note"
+            setTitle(getString(R.string.editor_activity_title_new_note));
+
+            // Invalidate the options menu, so the "Delete" menu option can be hidden.
+            // (It doesn't make sense to delete a note that hasn't been created yet.)
+            invalidateOptionsMenu();
+        } else {
+            // Otherwise this is an existing note, so change app bar to say "Edit Note"
+            setTitle(getString(R.string.editor_activity_title_edit_note));
+
+            // Initialize a loader to read the note data from the database and display the current
+            // values in the editor
+            getLoaderManager().initLoader(EXISTING_NOTE_LOADER, null, this);
+        }
+
         // Find relevant views that user input will be read from
         mTitleEditText = (EditText) findViewById(R.id.title);
         mNoteEditText = (EditText) findViewById(R.id.note);
+
+        // Setup OnTouchListeners on all the input fields, so we can determine if the user has
+        // touched or modified them. This will let us know if there are unsaved changes or not, if
+        // the user tries to leave the editor without saving.
+        mTitleEditText.setOnTouchListener(mTouchListener);
+        mNoteEditText.setOnTouchListener(mTouchListener);
     }
+
 
     /**
      * Get user input from editor and save a new note into the database.
      */
-    private void insertNote() {
+    private void saveNote() {
         // Read from input fields
         String titleString = mTitleEditText.getText().toString();
         String noteString = mNoteEditText.getText().toString();
+
+        // Check if this is supposed to be a new note and check if all the fields in the editor are
+        // blank
+        if (mCurrentNoteUri == null &&
+                TextUtils.isEmpty(titleString) && TextUtils.isEmpty(noteString)) {
+            // Since no fields were modified, we can return early without creating a new note.
+            // No need to create ContentValues and no need to do any ContentProvider operations.
+            return;
+        }
 
         // Create a ContentValues object where the column names are they keys, and the note's
         // content the values.
@@ -48,16 +121,38 @@ public class EditorActivity extends AppCompatActivity {
         values.put(NoteContract.NoteEntry.COLUMN_TITLE, titleString);
         values.put(NoteContract.NoteEntry.COLUMN_NOTE, noteString);
 
-        // Insert a new note into the provider, returning the content URI for the new note.
-        Uri newUri = getContentResolver().insert(NoteContract.NoteEntry.CONTENT_URI, values);
+        // Determine if this is a new or existing note by checking if mCurrentNoteUri is null or not
+        if (mCurrentNoteUri == null) {
+            // This is a NEW note, so insert a new note into the provider, returning the content URI
+            // for the new note.
+            Uri newUri = getContentResolver().insert(NoteContract.NoteEntry.CONTENT_URI, values);
 
-        // Show a toast message depending on whether or not the insertion was successful
-        if (newUri == null) {
-            // If the new content URI is null, then there was an error with insertion.
-            Toast.makeText(this, getString(R.string.editor_insert_note_failed), Toast.LENGTH_SHORT).show();
+            // Show a toast message depending on whether or not the insertion was successful
+            if (newUri == null) {
+                // If the new content URI is null, then there was an error with insertion.
+                Toast.makeText(this, getString(R.string.editor_insert_note_failed), Toast.LENGTH_SHORT).show();
+            } else {
+                // Otherwise, the insertion was successful and we can display a toast with the row ID.
+                Toast.makeText(this, getString(R.string.editor_insert_note_successful),
+                        Toast.LENGTH_SHORT).show();
+            }
         } else {
-            // Otherwise, the insertion was successful and we can display a toast with the row ID.
-            Toast.makeText(this, getString(R.string.editor_insert_note_successful), Toast.LENGTH_SHORT).show();
+            // Otherwise this is an EXISTING note, so update the note with content URI: mCurrentNoteUri
+            // and pass in the new ContentValues. Pass in null for the selection and selection args
+            // because mCurrentNoteUri will already identify the correct row in the database that
+            // we want to modify.
+            int rowsAffected = getContentResolver().update(mCurrentNoteUri, values, null, null);
+
+            // Show a toast message depending on whether or not the update was successful.
+            if (rowsAffected == 0) {
+                // If no rows were affected, then there was an error with the update.
+                Toast.makeText(this, getString(R.string.editor_update_note_failed),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                // Otherwise, the update was successful and we can display a toast.
+                Toast.makeText(this, getString(R.string.editor_update_note_successful),
+                        Toast.LENGTH_SHORT).show();
+            }
         }
     }
     @Override
@@ -68,6 +163,21 @@ public class EditorActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * This method is called after invalidateOptionsMenu(), so that the menu can be updated (some
+     * menu items can be hidden or made visible).
+     */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        // If this is a new note, hide the "Delete" menu item.
+        if (mCurrentNoteUri == null) {
+            MenuItem menuItem = menu.findItem(R.id.action_delete);
+            menuItem.setVisible(false);
+        }
+        return true;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // User clicked on a menu option in the app bar overflow menu
@@ -75,21 +185,198 @@ public class EditorActivity extends AppCompatActivity {
             // Respond to a click on the "Save" menu option
             case R.id.action_save:
                 // Save note to database
-                insertNote();
+                saveNote();
                 // Exit activity
                 finish();
                 return true;
             // Respond to a click on the "Delete" menu option
             case R.id.action_delete:
-                // Do nothing for now
+                // Pop up confirmation dialog for deletion
+                showDeleteConfirmationDialog();
                 return true;
             // Respond to a click on the "Up" arrow button in the app bar
             case android.R.id.home:
-                // Navigate back to parent activity (CatalogActivity)
-                NavUtils.navigateUpFromSameTask(this);
+                // Navigate back to parent activity {@link CatalogActivity}.
+                if (!mNoteHasChanged) {
+                    NavUtils.navigateUpFromSameTask(this);
                 return true;
+        }
+
+            // Otherwise if there are unsaved changes, setup a dialog to warn the user.
+            // Create a click listener to handle the user confirming that changes should be discarded.
+            DialogInterface.OnClickListener discardButtonClickListener =
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            // User clicked "Discard" button, navigate to parent activity.
+                            NavUtils.navigateUpFromSameTask(EditorActivity.this);
+                        }
+                    };
+
+            // Show a dialog that notifies the user they have unsaved changes
+            showUnsavedChangesDialog(discardButtonClickListener);
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * This method is called when the back button is pressed.
+     */
+    @Override
+    public void onBackPressed() {
+        // If the note hasn't changed, continue with handling back button press
+        if (!mNoteHasChanged) {
+            super.onBackPressed();
+            return;
+        }
+
+        // Otherwise if there are unsaved changes, setup a dialog to warn the user.
+        // Create a click listener to handle the user confirming that changes should be discarded.
+        DialogInterface.OnClickListener discardButtonClickListener =
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // User clicked "Discard" button, close the current activity.
+                        finish();
+                    }
+                };
+
+        // Show dialog that there are unsaved changes
+        showUnsavedChangesDialog(discardButtonClickListener);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Since the editor shows all note attributes, define a projection that contains all columns
+        // from the note table
+        String[] projection = {
+                NoteContract.NoteEntry._ID,
+                NoteContract.NoteEntry.COLUMN_TITLE,
+                NoteContract.NoteEntry.COLUMN_NOTE};
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                mCurrentNoteUri,         // Query the content URI for the current pet
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        // Bail early if the cursor is null or there is less than 1 row in the cursor
+        if (cursor == null || cursor.getCount() < 1) {
+            return;
+        }
+
+        // Proceed with moving to the first row of the cursor and reading data from it
+        // (This should be the only row in the cursor)
+        if (cursor.moveToFirst()) {
+            // Find the columns of note attributes that we're interested in
+            int titleColumnIndex = cursor.getColumnIndex(NoteContract.NoteEntry.COLUMN_TITLE);
+            int noteColumnIndex = cursor.getColumnIndex(NoteContract.NoteEntry.COLUMN_NOTE);
+
+            // Extract out the value from the Cursor for the given column index
+            String title = cursor.getString(titleColumnIndex);
+            String note = cursor.getString(noteColumnIndex);
+
+            // Update the views on the screen with the values from the database
+            mTitleEditText.setText(title);
+            mNoteEditText.setText(note);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // If the loader is invalidated, clear out all the data from the input fields.
+        mTitleEditText.setText("");
+        mNoteEditText.setText("");
+    }
+
+    /**
+     * Show a dialog that warns the user there are unsaved changes that will be lost if they
+     * continue leaving the editor.
+     *
+     * @param discardButtonClickListener is the click listener for what to do when
+     *                                   the user confirms they want to discard their changes
+     */
+    private void showUnsavedChangesDialog(
+            DialogInterface.OnClickListener discardButtonClickListener) {
+        // Create an AlertDialog.Builder and set the message, and click listeners for the positive
+        // and negative buttons on the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.unsaved_changes_dialog_msg);
+        builder.setPositiveButton(R.string.discard, discardButtonClickListener);
+        builder.setNegativeButton(R.string.keep_editing, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Keep editing" button, so dismiss the dialog and continue
+                // editing the note.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    /**
+     * Prompt the user to confirm that they want to delete this note.
+     */
+    private void showDeleteConfirmationDialog() {
+        // Create an AlertDialog.Builder and set the message, and click listeners for the positive
+        // and negative buttons on the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.delete_dialog_msg);
+        builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Delete" button, so delete the pet.
+                deletePet();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Cancel" button, so dismiss the dialog and continue editing the
+                // note.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    /**
+     * Perform the deletion of the note in the database.
+     */
+    private void deleteNote() {
+        // Only perform the delete if this is an existing pet.
+        if (mCurrentNoteUri != null) {
+            // Call the ContentResolver to delete the note at the given content URI.
+            // Pass in null for the selection and selection args because the mCurrentNoteUri
+            // content URI already identifies the note that we want.
+            int rowsDeleted = getContentResolver().delete(mCurrentNoteUri, null, null);
+
+            // Show a toast message depending on whether or not the delete was successful.
+            if (rowsDeleted == 0) {
+                // If no rows were deleted, then there was an error with the delete.
+                Toast.makeText(this, getString(R.string.editor_delete_note_failed),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                // Otherwise, the delete was successful and we can display a toast.
+                Toast.makeText(this, getString(R.string.editor_delete_note_successful),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        // Close the activity
+        finish();
+    }
 }
