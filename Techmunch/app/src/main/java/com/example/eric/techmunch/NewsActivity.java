60c7fee283eace7f18c21aa4e5e557c6de26e567
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-10-18 13:27:28 +0200

    Add Loader to NewsActivity

--- Techmunch/app/src/main/java/com/example/eric/techmunch/NewsActivity.java
+++ Techmunch/app/src/main/java/com/example/eric/techmunch/NewsActivity.java
@@ -1,8 +1,14 @@
 package com.example.eric.techmunch;
 
+
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,11 +18,18 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewsActivity extends AppCompatActivity {
+public class NewsActivity extends AppCompatActivity
+        implements LoaderCallbacks<List<News>> {
 
     // URL for news data from the Guardian API
     private static final String REQUEST_URL =
             "https://content.guardianapis.com/search?q=technology&api-key=test";
+
+    /**
+     * Constant value for the news loader ID. We can choose any integer. This really only comes into
+     * play when you're using multiple loaders.
+     */
+    private static final int NEWS_LOADER_ID = 1;
 
     // Adapter for the list of news stories
     private NewsAdapter mAdapter;
@@ -54,56 +67,35 @@ public class NewsActivity extends AppCompatActivity {
             }
         });
 
-        // Start the AsyncTask to fetch the news data
-        NewsAsyncTask task = new NewsAsyncTask();
-        task.execute(REQUEST_URL);
+        // Get a reference to the LoaderManager, in order to interact with loaders.
+        LoaderManager loaderManager = getSupportLoaderManager();
+        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+        // because this activity implements the LoaderCallbacks interface).
+        loaderManager.initLoader(NEWS_LOADER_ID, null, this);
     }
 
-    /**
-     * {@link AsyncTask} to perform the network request on a background thread, and then update the
-     * UI with the list of news in the response.
-     *
-     * AsyncTask has three generic parameters: the input type, a type used for progress updates, and
-     * an output type. Our task will take a String URL, and return an news story. We won't do
-     * progress updates, so the second generic is just Void.
-     *
-     * We'll only override two of the methods of AsyncTask: doInBackground() and onPostExecute().
-     * The doInBackground() method runs on a background thread, so it can run long-running code
-     * (like network activity), without interfering with the responsiveness of the app.
-     * Then onPostExecute() is passed the result of doInBackground() method, but runs on the
-     * UI thread, so it can use the produced data to update the UI.
-     */
-    private class NewsAsyncTask extends AsyncTask<String, Void, List<News>> {
-        /**
-         * This method runs on a background thread and performs the network request.
-         * We should not update the UI from a background thread, so we return a list of
-         * {@link News} as the result.
-         */
-        @Override
-        protected List<News> doInBackground(String... urls) {
-            // Don't perform the request if there are no URLs, or the first URL is null
-            if (urls.length < 1 || urls[0] == null) {
-                return null;
-            }
-            List<News> result = QueryUtils.fetchNewsData(urls[0]);
-            return result;
+    @Override
+    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
+        // Create a new loader for given URL
+        return new NewsLoader(this, REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> news) {
+        // Clear adapter of previous news data
+        mAdapter.clear();
+
+        // If there is a valid list of {@link News}, then add them to the adapter's data set.
+        // This will trigger the ListView to update.
+        if (news != null && !news.isEmpty()) {
+            mAdapter.addAll(news);
         }
-        /**
-         * This method runs on the main UI thread after the background work has been completed.
-         * It receives as input, the return value from the doInBackground() method. First we clear
-         * out the adapter, to get rid of news data from a previous query. Then we update the
-         * adapter with the new list of news, which will trigger the ListView to re-populate its
-         * list items.
-         */
-        @Override
-        protected void onPostExecute(List<News> data) {
-            // Clear the adapter of previous news data
-            mAdapter.clear();
-            // If there is a valid list of {@link News}, then add them to the adapter's data set.
-            // This will trigger the ListView to update.
-            if (data != null && !data.isEmpty()) {
-                mAdapter.addAll(data);
-            }
-        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
     }
 }
