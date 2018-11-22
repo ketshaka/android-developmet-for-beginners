5ab53202332d3e24cc93d66fac0774613858218b
Tshaka Eric Lekholoane <23554017+ketshaka@users.noreply.github.com>
2018-11-22 14:45:22 +0200

    Remove unused library calls

--- RugbyScoring/app/src/main/java/com/example/android/rugbyscoring/MainActivity.java
+++ RugbyScoring/app/src/main/java/com/example/android/rugbyscoring/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.android.rugbyscoring;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 /**
