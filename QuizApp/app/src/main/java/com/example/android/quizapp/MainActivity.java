49b9d16175fea26663a930e99ecc21b27db85627
Eric Lekholoane <23554017+Leveson@users.noreply.github.com>
2019-11-27 17:24:37 +0200

    Update QuizApp
    Update naming and other strings for clarity and correction.

--- QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button submit;
+    Button submitButton;
     int score = 0;
 
     @Override
@@ -19,57 +19,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        submit = (Button) findViewById(R.id.submitButton);
-        submit.setOnClickListener(submitButtonOnClickListener);
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(submitButtonOnClickListener);
     }
 
-    private void checkQuesOne(){
-        RadioButton radioButtonYes = (RadioButton) findViewById(R.id.radioTrue);
-        boolean isQuesOneYesChecked = radioButtonYes.isChecked();
-        if (isQuesOneYesChecked){
+    private void checkQuestionOne(){
+        RadioButton radioButtonTrue = (RadioButton) findViewById(R.id.radioTrue);
+        boolean isQuestionOneYesChecked = radioButtonTrue.isChecked();
+        if (isQuestionOneYesChecked){
             score += 1;
         }
     }
 
-    private String getQuesTwoInput() {
-        EditText input = (EditText) findViewById(R.id.editQues2);
-        String algo = input.getText().toString();
-        return algo;
+    private String getQuestionTwoTextInput() {
+        EditText editText = (EditText) findViewById(R.id.editQues2);
+        String textInput = editText.getText().toString();
+        return textInput;
     }
 
-    private void checkQuesTwo(){
-        String algo = getQuesTwoInput();
-        if (algo.trim().equalsIgnoreCase(getString(R.string.q2_answer))){
+    private void checkQuestionTwo(){
+        String textInput = getQuestionTwoTextInput();
+        if (textInput.trim().equalsIgnoreCase(getString(R.string.q2_answer))){
             score += 1;
         }
     }
 
-    private void checkQuesThree(){
-        CheckBox quesThreeQueues = (CheckBox) findViewById(R.id.checkQueues);
-        CheckBox quesThreeStacks = (CheckBox) findViewById(R.id.checkStacks);
-        CheckBox quesThreeTrees = (CheckBox) findViewById(R.id.checkTrees);
-        boolean isQuesThreeQueuesChecked = quesThreeQueues.isChecked();
-        boolean isQuesThreeStacksChecked = quesThreeStacks.isChecked();
-        boolean isQuesThreeTreesChecked = quesThreeTrees.isChecked();
+    private void checkQuestionThree(){
+        CheckBox checkBoxQueues = (CheckBox) findViewById(R.id.checkQueues);
+        CheckBox checkBoxStacks = (CheckBox) findViewById(R.id.checkStacks);
+        CheckBox checkBoxTrees = (CheckBox) findViewById(R.id.checkTrees);
+        boolean isCheckBoxQueuesChecked = checkBoxQueues.isChecked();
+        boolean isCheckBoxStacksChecked = checkBoxStacks.isChecked();
+        boolean isCheckBoxTreesChecked = checkBoxTrees.isChecked();
 
-        if (isQuesThreeQueuesChecked && isQuesThreeStacksChecked && !isQuesThreeTreesChecked){
+        if (isCheckBoxQueuesChecked && isCheckBoxStacksChecked && !isCheckBoxTreesChecked){
             score += 1;
         }
     }
 
-    private void checkQuesFour(){
-        RadioButton radioButtonBubble = (RadioButton) findViewById(R.id.radioBubble);
-        boolean isQuesFourBubbleChecked = radioButtonBubble.isChecked();
-        if (isQuesFourBubbleChecked){
+    private void checkQuestionFour(){
+        RadioButton radioButtonBubbleSort = (RadioButton) findViewById(R.id.radioBubble);
+        boolean isQuestionFourBubbleSortChecked = radioButtonBubbleSort.isChecked();
+        if (isQuestionFourBubbleSortChecked){
             score += 1;
         }
     }
 
-    private void checkQues(){
-        checkQuesOne();
-        checkQuesTwo();
-        checkQuesThree();
-        checkQuesFour();
+    private void checkQuestions(){
+        checkQuestionOne();
+        checkQuestionTwo();
+        checkQuestionThree();
+        checkQuestionFour();
     }
 
     private void resetScore(){
@@ -78,8 +78,8 @@ public class MainActivity extends AppCompatActivity {
 
     final View.OnClickListener submitButtonOnClickListener = new View.OnClickListener() {
         public void onClick(final View v){
-            checkQues();
-            Toast.makeText(MainActivity.this, getString(R.string.whoop) + score + getString(R.string.over_4),
+            checkQuestions();
+            Toast.makeText(MainActivity.this, "Whoop! Your score is: " + score + "/4",
                     Toast.LENGTH_LONG).show();
             resetScore();
         }
