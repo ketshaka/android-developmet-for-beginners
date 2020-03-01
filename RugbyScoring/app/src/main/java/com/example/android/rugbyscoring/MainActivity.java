0cfa03f4f69e47129a9c90a6ce390b2b12cc0341
Eric Lekholoane <23554017+Leveson@users.noreply.github.com>
2020-03-02 00:05:44 +0200

    Update MainActivity.java

--- RugbyScoring/app/src/main/java/com/example/android/rugbyscoring/MainActivity.java
+++ RugbyScoring/app/src/main/java/com/example/android/rugbyscoring/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
  * This activity keeps track of the score for a rugby game.
  */
 public class MainActivity extends AppCompatActivity {
-
     // Tracks the score for Team A
     int scoreTeamA = 0;
 
@@ -22,57 +21,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * Increase the score for Team A by 2 points (conversion).
-     */
+    // Increase the score for Team A by 2 points (conversion).
     public void addTwoForTeamA(View v) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
-    /**
-     * Increase the score for Team A by 3 points (penalty or drop goal).
-     */
+    // Increase the score for Team A by 3 points (penalty or drop goal).
     public void addThreeForTeamA(View v) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
 
-    /**
-     * Increase the score for Team A by 5 points (try).
-     */
+    // Increase the score for Team A by 5 points (try).
     public void addFiveForTeamA(View v) {
         scoreTeamA = scoreTeamA + 5;
         displayForTeamA(scoreTeamA);
     }
 
-    /**
-     * Increase the score for Team B by 2 points (conversion).
-     */
+    // Increase the score for Team B by 2 points (conversion).
     public void addTwoForTeamB(View v) {
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
     }
 
-    /**
-     * Increase the score for Team B by 3 points (penalty or drop goal).
-     */
+    // Increase the score for Team B by 3 points (penalty or drop goal).
     public void addThreeForTeamB(View v) {
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
     }
 
-    /**
-     * Increase the score for Team B by 5 points (try).
-     */
+    // Increase the score for Team B by 5 points (try).
     public void addFiveForTeamB(View v) {
         scoreTeamB = scoreTeamB + 5;
         displayForTeamB(scoreTeamB);
     }
 
-    /**
-     * Resets the score for both teams back to 0 (new game).
-     */
+    // Resets the score for both teams back to 0 (new game).
     public void resetScore(View v) {
         scoreTeamA = 0;
         scoreTeamB = 0;
@@ -80,17 +65,13 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamB(scoreTeamB);
     }
 
-    /**
-     * Updates the score for Team A.
-     */
+    // Updates the score for Team A.
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
-    /**
-     * Updates the score for Team B.
-     */
+    // Updates the score for Team B.
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
